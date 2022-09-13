3e78b433029a433b185c47a56ae32ec83449f6ed
alperenkavukk <kavuk.67.76@gmail.com>
2022-09-13 15:21:56 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JokesAppSwiftUI
+//
+//  Created by Alperen Kavuk on 13.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Alperen Kavuk on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
